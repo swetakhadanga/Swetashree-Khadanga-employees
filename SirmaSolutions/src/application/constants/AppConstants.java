1e00ffb7c170aa96bbfbea4bc8c44d2b61011d86
sweta <sweta@DESKTOP-IUQG1OC>
2022-12-24 18:53:04 +0530

    This is my 1st commit.

--- SirmaSolutions/src/application/constants/AppConstants.java
+++ SirmaSolutions/src/application/constants/AppConstants.java
@@ -6,7 +6,7 @@ public class AppConstants {
     private AppConstants() {
     }
 
-    public static final String FILE_PATH = ".\\src\\Resources\\emp.txt";
+    public static final String FILE_PATH = "C:\\Users\\sweta\\git\\repository\\SirmaSolutions\\src\\Resources\\emp.txt";
 
     public static final String TEAM_LONGEST_PERIOD = "The pair of employees who have worked together on common projects for the longest period of time are:%n employeeID: %d & employeeID: %d with total overlap duration %d days";
     public static final String NO_TEAMS_MSG = "Doesn't exist pair of employees which have worked together on common projects.";
